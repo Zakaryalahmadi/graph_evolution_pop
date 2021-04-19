d3bc8074008744957c6e0aa5645b6fc42552085e
keyz93 <80518856+keyz93@users.noreply.github.com>
2021-04-19 15:33:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 from pylab import *
-import db
 
 
-import exceptions
-
-def readData(filename):
+# fonction pour importer les donnees qu'il y a dans un fichier
+def lireDonnee(nom_fichier):
     table = []
-    fData = open(filename)
+    fData = open(nom_fichier)
 
     while True:
         line = fData.readline().replace(" ", "").replace("'", "")
         if line == "":
             break
-        line = line[:-1] # remove trailing \n
-        try:
-            q = float(line)
-        except exceptions.ValueError:
-           break
+        line = line[:-1] # supprimer le '\n'
+        
+        q = float(line) 
+        
         table.append(q)
     fData.close()
     return table
 
+pop_list = lireDonnee("/home/lahmadi/Téléchargements/evolution_pop.txt")
 
-x = array(db.list_annee)
-y = array(db.list_population)
+list_annee = []
+annee = 1959
+for x in range(60):
+  annee += 1
+  list_annee.append(annee)
+  
+
+  
+x = array(list_annee)
+y = array(pop_list)
 ylabel("évolution de la population (en milliard)")
 
 xlabel("années")
