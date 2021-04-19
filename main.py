bd35acc076fb7ae11cfa10b89c3eee6f7e3edaf2
keyz93 <80518856+keyz93@users.noreply.github.com>
2021-04-19 15:20:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,24 @@ from pylab import *
 import db
 
 
+import exceptions
+
+def readData(filename):
+    table = []
+    fData = open(filename)
+
+    while True:
+        line = fData.readline().replace(" ", "").replace("'", "")
+        if line == "":
+            break
+        line = line[:-1] # remove trailing \n
+        try:
+            q = float(line)
+        except exceptions.ValueError:
+           break
+        table.append(q)
+    fData.close()
+    return table
 
 
 x = array(db.list_annee)
