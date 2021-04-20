56d5ea89f081552de481655495d96c22517fcb06
keyz93 <80518856+keyz93@users.noreply.github.com>
2021-04-20 15:00:12 +0200

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,35 @@
+"""
+Created on Mon Apr 19 18:42:56 2021
+
+@author: lahmadi
+"""
+
+def lireDonnee(nom_fichier):
+    """
+        Fonction qui permet de recuperer les données d'un fichier dans une liste
+    """
+    
+    table = []
+    fData = open(nom_fichier)
+
+    while True:
+        line = fData.readline().replace(" ", "").replace("'", "")
+        if line == "":
+            break
+        line = line[:-1] # supprimer le '\n'
+        
+        q = float(line) 
+        
+        table.append(q)
+    fData.close()
+    return table
+
+
+def year_interval(annee_debut, annee_fin):
+    list_annee = []
+    annee = annee_debut
+    for x in range(annee_fin - annee_debut):
+        list_annee.append(annee)
+        annee += 1
+    return list_annee
+      
