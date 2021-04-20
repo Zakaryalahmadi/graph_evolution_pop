a1e4a2a1aa1d2d0f61bb4d78a74858bbb9338f34
keyz93 <80518856+keyz93@users.noreply.github.com>
2021-04-20 15:15:21 +0200

    Update taux_croissance.py

--- taux_croissance.py
+++ taux_croissance.py
@@ -1,12 +1,12 @@
-import pylab
+from pylab import *
 from modules import *
 
 
-pop_list = lireDonnee("data/evolution_pop.txt")
+pop_list = lireDonnee("data/taux_croissance.txt")
 
 
 
-list_annee = year_interval(1960, 2020)
+list_annee = year_interval(1960, 2019)
 
   
 x = array(list_annee)
